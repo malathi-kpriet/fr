3f11bf858671274f5f67f83aeb4208d951e00a19
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-16 16:15:34 +0530

    finish

---
+++ README.c
@@ -0,0 +1,16 @@
+# fr
+#include <stdio.h>
+int main() 
+  {
+    int a[10],i,n,sum=0,avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    	scanf("%d",&a[i]);
+     for(i=0;i<n;i++)
+     {
+     	sum+=a[i];
+     }
+     avg=sum/n;
+     printf("%d",avg);
+    return 0;
+  }
